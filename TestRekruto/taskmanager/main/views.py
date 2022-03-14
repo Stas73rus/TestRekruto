7c903b7fb744cecafc8f83c620fc81fa9badc3d4
Stanislav <stanislav-09@inbox.ru>
2022-03-14 23:31:51 +0400

    TestRekruto

---
+++ TestRekruto/taskmanager/main/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def infoRekruto(request):
+    name = request.GET.get('name', 'Rekruto')
+    message = request.GET.get('message', 'Давай дружить!')
+    start_message = request.GET.get('start_message', 'Rekruto! Давай дружить!')
+    list_get_query = {
+        'name': name,
+        'message': message,
+        'start_message': start_message,
+    }
+    template = 'main/index.html'
+    return render(request, template, list_get_query)
